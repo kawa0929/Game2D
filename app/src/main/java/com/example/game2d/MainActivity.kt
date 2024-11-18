28f02cf296a23cbeff2d61eb3eaf83892e063cb0
student <melisaqq55@gmail.com>
2024-11-18 16:19:06 +0800

    OK

--- app/src/main/java/com/example/game2d/MainActivity.kt
+++ app/src/main/java/com/example/game2d/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.game2d.ui.theme.Game2DTheme
 import android.content.pm.ActivityInfo
+import androidx.compose.foundation.Image
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -22,8 +23,27 @@ import androidx.compose.material3.Button
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.delay
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.layout.offset
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.IntOffset
 
 
+
+import kotlin.math.abs
+class Background(val screenW:Int) {
+    var x1 = 0  //背景圖1_x軸
+    var x2 = screenW  //背景圖2_x軸
+
+    fun Roll(){
+        x1 --
+        if (abs(x1) > screenW){ //已經移動整張
+            x1 = 0
+        }
+        x2 = x1 + screenW
+    }
+}
 
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +54,11 @@ class MainActivity : ComponentActivity() {
             Game2DTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
-                    Start(m = Modifier.padding(innerPadding))
+                    val screenW = resources.displayMetrics.widthPixels
+                    val screenH = resources.displayMetrics.heightPixels
+                    val game = Game(GlobalScope,screenW, screenH)
+                    Start(m = Modifier.padding(innerPadding),game, screenW)
+
                 }
             }
         }
@@ -42,22 +66,66 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Start(m: Modifier) {
-    var counter by remember { mutableStateOf(0) }
+fun Start(m: Modifier, game:Game, screenW:Int) {
+    var msg by remember { mutableStateOf("遊戲開始") }
+    val counter by game.state.collectAsState()
+    var counter2 by remember { mutableStateOf(0) }
+
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖",
+        contentScale = ContentScale.FillBounds,  //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(game.background.x1, 0) }
+
+
+    )
+    Image(
+        painter = painterResource(id = R.drawable.forest),
+        contentDescription = "背景圖2",
+        contentScale = ContentScale.FillBounds,  //縮放符合螢幕寬度
+        modifier = Modifier
+            .offset { IntOffset(game.background.x2, 0) }
+
+
+    )
+
+
+
+
+
     Row {
         Button(
             onClick = {
-                GlobalScope.launch {
-                    counter = 0
-                    while (counter<1000){
-                        delay(40)
-                        counter ++
-                    }
+                if (msg=="遊戲開始"){
+                    msg = "遊戲暫停"
+                    game.Play()
                 }
-            }
+                else{
+                    msg = "遊戲開始"
+                    game.isPlaying = false
+                }
+
+
+            },
+            modifier = m
         ) {
-            Text(text = "開始")
+            Text(text = msg)
         }
-        Text(text = counter.toString(), modifier = m)
+        Text(text = "%.2f 秒".format(counter*.04), modifier = m)
+
+        Button(
+            onClick = {
+                counter2++
+
+            },
+            modifier = m
+        ) {
+            Text(text = "加一")
+        }
+            Text(text = counter2.toString(), modifier = m)
+        }
     }
-}
+
+
+
