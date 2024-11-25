f9ec731ca28c2ffcb42b507dd17e6110e5c4b329
student <melisaqq55@gmail.com>
2024-11-25 13:27:47 +0800

    1125-1

--- app/src/main/java/com/example/game2d/MainActivity.kt
+++ app/src/main/java/com/example/game2d/MainActivity.kt
@@ -28,21 +28,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.foundation.layout.offset
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.IntOffset
-import kotlin.math.abs
 
-
-class Background(val screenW:Int) {
-    var x1 = 0  //背景圖1_x軸
-    var x2 = screenW  //背景圖2_x軸
-
-    fun Roll(){
-        x1 --
-        if (abs(x1) > screenW){ //已經移動整張
-            x1 = 0
-        }
-        x2 = x1 + screenW
-    }
-}
 
 
 class MainActivity : ComponentActivity() {
